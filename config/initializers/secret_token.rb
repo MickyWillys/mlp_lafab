97718a6b1fe28732f2285b9b605e8c61d04036d7
MickyWillys <fozeum@yahoo.fr>
2014-03-12 10:20:05 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MlpLafab::Application.config.secret_key_base = 'ba9b03419f40b81ca6bdbbfc5e4c04b6b54e632c7cd9732424b42449866e2abc1becfb01ef0476b789e6b90c94c959e162c7f126d61e61beeea61cdb2175079e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MlpLafab::Application.config.session_store :cookie_store, key: '_mlp_lafab_session'

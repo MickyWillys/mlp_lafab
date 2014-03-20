f71d5bb2f5eb027addf3e90c4c0bc96de62c3f47
MickyWillys <fozeum@yahoo.fr>
2014-03-20 15:38:38 +0100

    first slide almost done

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ MlpLafab::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
+  root 'static_pages#landing'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

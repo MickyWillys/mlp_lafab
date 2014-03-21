955a2da774d82d068db36254ec39ac8c711afc13
MickyWillys <fozeum@yahoo.fr>
2014-03-21 18:44:33 +0100

    Attribution user_id a une skill

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,7 +1,27 @@
 class SkillsController < ApplicationController
+	
 	def new
+		# Attribution de l'user_id à la skill
+		# Nb ON DOIT ETRE CONNECTE
+		@user= current_user
 		@skill= Skill.new
+		@skill.update_attributes(user_id: @user.id)
 	end
 
+
+	def create
+		@skill = Skill.create(skills_params)
+		if @skill.save
+			flash[:success] = "skills sauvegardées!"
+			redirect_to root_url
+		else
+			render 'static_pages/home'
+		end
+	end
+
+	private
+	def skill_params
+		params.require(:skill).permit(:user_id, :title, :tag_list)
+	end
 
 end

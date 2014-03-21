3a0659f84bdaf3081e93329787f04da60b35b493
MickyWillys <fozeum@yahoo.fr>
2014-03-21 15:53:53 +0100

    Ajout de devise termine - tout fonctionne

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 MlpLafab::Application.routes.draw do
   devise_for :users
+  resources :users
   get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
